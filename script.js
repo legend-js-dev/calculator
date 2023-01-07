ac4ea28fdff2bef809e809d0386c5387d60650d4
legendjs <75608223+legend-js-dev@users.noreply.github.com>
2023-01-07 22:13:59 +0545

    Upload File

---
+++ script.js
@@ -0,0 +1,30 @@
+let typed = document.getElementById("calc-typed");
+let operation = document.getElementById("calc-operation");
+
+function enter(str) {
+  if (typed.innerHTML === "0") typed.innerHTML = str; else typed.innerHTML += str;
+  if (operation.innerHTML) operation.innerHTML = "";
+}
+
+function clearAll() {
+  console.log("eh")
+  typed.innerHTML = "0";
+  operation.innerHTML = "";
+}
+
+function back() {
+  typed.innerHTML = typed.innerHTML.substring(0, typed.innerHTML.length - 1);
+  if (!typed.innerHTML) typed.innerHTML = "0";
+}
+
+function negate() {
+  let matches = typed.innerHTML.match(/[0-9]*\.?[0-9]+$/);
+  console.log(matches)
+  typed.innerHTML = typed.innerHTML.split(matches[0]).join("(-" + matches[0] + ")");
+}
+
+function equals() {
+  let answer = math.evaluate(typed.innerHTML);
+  operation.innerHTML = typed.innerHTML + " =";
+  typed.innerHTML = answer;
+}
